37644426366d3502385104928210f94d576dc888
Christoffer Gretarsson Alexandersen <cga323@gmail.com>
2024-08-07 18:22:25 -0400

    small stuff

--- scripts/infer_ode.jl
+++ scripts/infer_ode.jl
@@ -5,11 +5,8 @@ Infer parameters of ODE using Bayesian framework
 =#
 
 # DIFFUSION, RETRO- AND ANTEROGRADE
-#priors = OrderedDict( "σ" => LogNormal(0,1), "ρ" => truncated(Normal(0,0.1), lower=0.), "seed" => truncated(Normal(1.,0.1),lower=0) );
-#thresholds = [0.15, 0.05, 0.01, 0.0];
-#Ns = [40, 95, 174, 366];
-thresholds = [0.15];
-Ns = [174];
+thresholds = [0.15, 0.05, 0.01, 0.0];
+Ns = [40, 95, 174, 366];
 for i in eachindex(thresholds)
     println("Inferring N=$(Ns[i])")
     priors2 = OrderedDict( "σ" => LogNormal(0,1), "ρₐ" => truncated(Normal(0,0.01), lower=0.), "ρᵣ" => truncated(Normal(0,0.01), lower=0.), "seed" => truncated(Normal(1.,0.1),lower=0) );
@@ -19,16 +16,16 @@ for i in eachindex(thresholds)
                     "data/avg_total_path.csv",
                     "data/timepoints.csv", 
                     "data/W_labeled.csv"; 
-                    n_threads=1,
+                    n_threads=4,
                     threshold=thresholds[i],
                     abstol=1e-6,
                     reltol=1e-3,
                     adtype=AutoReverseDiff(compile=true),
                     sensealg=InterpolatingAdjoint(autojacvec=ReverseDiffVJP(true)),
-                    benchmark=true,
+                    benchmark=false,
                     benchmark_ad=[:reversediff,:reversediff_compiled]
                     )
 
     # save inference result
-    #serialize("simulations/total_diffusion2_N=$(Ns[i]).jls", inference)
+    serialize("simulations/total_diffusion2_N=$(Ns[i]).jls", inference)
 end
