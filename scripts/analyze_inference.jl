248c3151cae990619ca84f17dd1662222ce5e407
gretarsson <cga323@gmail.com>
2024-08-08 10:39:49 -0400

    produced plotsw

--- scripts/analyze_inference.jl
+++ scripts/analyze_inference.jl
@@ -4,7 +4,7 @@ using MCMCChains
 include("helpers.jl");
 
 # load inference simulation 
-simulation = "total_diffusion2_N=40";
+simulation = "total_diffusion2_N=95";
 inference = deserialize("simulations/"*simulation*".jls")
 
 # plot pred. vs obsv., chains, posterior distributions, and retrodiction
@@ -15,3 +15,5 @@ prior_figs = plot_priors(inference; save_path=save_path*"/priors");
 posterior_figs = plot_posteriors(inference, save_path=save_path*"/posteriors");
 retrodiction_figs = plot_retrodiction(inference; save_path=save_path*"/retrodiction");
 prior_and_posterior_figs = plot_prior_and_posterior(inference; save_path=save_path*"/prior_and_posterior");
+
+inference["chain"]
