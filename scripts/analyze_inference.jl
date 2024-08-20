9473343a9a3b9c9b06548fd8f4dfc669df842179
Christoffer Gretarsson Alexandersen <cga323@gmail.com>
2024-08-20 10:12:57 -0400

    plotted datadcit simulations

--- scripts/analyze_inference.jl
+++ scripts/analyze_inference.jl
@@ -5,9 +5,14 @@ include("helpers.jl");
 
 
 # simulation to analyze
-simulation = "total_death_N=174";
+simulation = "total_death_N=95_datadict";
+simulation2 = "total_death_N=95";
+
 
 # plot 
 inference = deserialize("simulations/"*simulation*".jls")
+inference2 = deserialize("simulations/"*simulation2*".jls")
+inference["data"] = inference2["data"]
+
 
 plot_inference(inference,"figures/"*simulation;plotscale=log10)
