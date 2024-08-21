f88e43598000f65eb4c77f54c99e08d7626469f8
Christoffer Gretarsson Alexandersen <cga323@gmail.com>
2024-08-21 13:04:01 -0400

    miniupdate

--- scripts/analyze_inference.jl
+++ scripts/analyze_inference.jl
@@ -9,16 +9,4 @@ simulation = "test_datadict";
 
 # plot 
 inference = deserialize("simulations/"*simulation*".jls")
-#data = inference["data"]
-#var_data = var3(data)
-#mean_data = mean3(data)
-#var(skipmissing(data[1,4,:]))
-#var_data[1,4]
-#mean_data[1,4]
-#var_data_i = var_data[1,:]
-#indices = findall(x -> isnan(x),var_data_i)
-#var_data_i[indices] .= 0
-#var_data_i
-
-
 plot_inference(inference,"figures/"*simulation;plotscale=log10) 
