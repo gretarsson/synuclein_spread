c503be1483c60db9ccae49b5297e4cbaa253e433
gretarsson <cga323@gmail.com>
2024-08-28 16:43:26 -0400

    small update

--- scripts/analyze_inference.jl
+++ scripts/analyze_inference.jl
@@ -5,7 +5,7 @@ include("helpers.jl");
 
 
 # simulation to analyze
-simulation = "total_diffusion2_N=448_ultimate";
+simulation = "total_diffusion2_N=40";
 
 # plot 
 inference = deserialize("simulations/"*simulation*".jls")
