ada5815d9a39258846a4add9f7b76d380e96255f
gretarsson <cga323@gmail.com>
2025-01-14 15:32:12 -0500

    new sims

--- scripts/gene_analysis.jl
+++ scripts/gene_analysis.jl
@@ -12,9 +12,9 @@ gene_labels = readdlm("data/avg_Pangea_exp.csv",',')[1,2:end];  # names of genes
 # pick simulation and parameter
 simulation = "simulations/total_death_simplifiedii_N=448_threads=1_var1_normalpriors.jls";
 parameter = "β";
-file_name = "gene_significance";
-S = 10000;  # number of iterations
-null = false;
+file_name = "gene_significance_null";
+S = 1000;  # number of iterations
+null = true;
 
 # Find significant genes in each iterate from posterior
 significants = Vector{Any}(undef, S);
