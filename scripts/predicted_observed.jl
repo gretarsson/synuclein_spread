dbaca83a8d1c3a6327cdcba863dc92be14b84c39
gretarsson <cga323@gmail.com>
2025-03-21 12:48:41 -0400

    added plots to look at prior vs posterior means and std over regions

--- scripts/predicted_observed.jl
+++ scripts/predicted_observed.jl
@@ -6,6 +6,5 @@ using CSV
 simulation = "total_death_simplifiedii_bilateral_N=444_threads=1_var1";
 inference_obj = deserialize("simulations/"*simulation*".jls")
 
-
 # predicted vs observed plots with correlation
 figs = predicted_vs_observed_plots(inference_obj; save_path="figures/predicted_observed_retro", plotscale=:log10, remove_zero=true, aspect_ratio=:equal, plot_identity_line=true, plot_fit_line=false)
