dbd025c3e18bfb702ceb73f661766a1214bd5d1f
gretarsson <cga323@gmail.com>
2025-04-14 18:10:57 -0400

    new pltos

--- scripts/analyze_inference.jl
+++ scripts/analyze_inference.jl
@@ -4,9 +4,10 @@ here we create a folder of analysis plots of interence results
 include("helpers.jl");
 
 # simulation to analyze
-simulation = "total_fastslow_N=40_threads=1_var1_synthetic_smallgamma";
+simulation = "total_sir_N=40_threads=1_var1";
 
 # plot 
 inference_obj = deserialize("simulations/"*simulation*".jls")
+inference_obj["data_indices"]
 
 plot_inference(inference_obj,"figures/"*simulation;plotscale=log10)  
