19ffd4a7bd319a03dcac560383b19b5e8677bacd
gretarsson <cga323@gmail.com>
2025-04-29 20:30:19 -0400

    s

--- scripts/distr_infer.jl
+++ scripts/distr_infer.jl
@@ -26,39 +26,37 @@ ode = DIFFGAM;
 n_threads = 1;
 
 # READ DATA
+Lr,N,labels = read_W("data/W_labeled.csv", direction=:retro);
+La,_,_ = read_W("data/W_labeled.csv", direction=:antero);
+Ltuple = (Lr,N)  # order is (L,N) or (Lr, La, N)
 timepoints = vec(readdlm("data/timepoints.csv", ','));
 data = deserialize("data/total_path_3D.jls");
-Lr,N,labels = read_W("data/W_labeled_filtered.csv", direction=:retro);
-La,_,_ = read_W("data/W_labeled_filtered.csv", direction=:antero);
-Ltuple = (Lr,N)  # order is (L,N) or (Lr, La, N)
 seed = findfirst(==("iCP"), labels);
 
 # SET PRIORS
 K = N  # number of regional parameters
 display("N = $(N)")
-u0 = [0. for _ in 1:(2*N)];  # adaptation
-#u0 = [0. for _ in 1:(N)];  # without adaptation
+#u0 = [0. for _ in 1:(2*N)];  # adaptation
+u0 = [0. for _ in 1:(N)];  # without adaptation
 
 # DEFINE PRIORS
 priors = OrderedDict{Any,Any}( "ρ" => truncated(Normal(0,0.1),lower=0) ); 
 #priors = OrderedDict{Any,Any}( "ρr" => truncated(Normal(0,0.1),lower=0), "ρa" => truncated(Normal(0,0.1),lower=0) ); 
 priors["α"] = truncated(Normal(0,0.1),lower=0);
 for i in 1:K
-    #priors["β[$(i)]"] = Normal(0,1);
-    priors["β[$(i)]"] = truncated(Normal(0,1),lower=0);
+    priors["β[$(i)]"] = Normal(0,1);
+    #priors["β[$(i)]"] = truncated(Normal(0,1),lower=0);
 end
-for i in 1:K
-    #priors["d[$(i)]"] = Normal(0,1);
-    priors["d[$(i)]"] = truncated(Normal(0,1),lower=0);
-end
-priors["γ"] = truncated(Normal(0,1),lower=0)
-priors["λ"] = Normal(0,1)
+#for i in 1:K
+#    #priors["d[$(i)]"] = Normal(0,1);
+#    priors["d[$(i)]"] = truncated(Normal(0,1),lower=0);
+#end
 #priors["σ"] = LogNormal(0,1);
 priors["σ"] = filldist(LogNormal(0,1),N);
 priors["seed"] = truncated(Normal(0,0.1),lower=0);
 #
 # parameter refactorization
-factors = [1., 1., [1 for _ in 1:K]..., [1 for _ in 1:K]..., 1., 1.];  # death
+factors = [1., 1., [1 for _ in 1:K]...];  # death
 
 
 # INFER
