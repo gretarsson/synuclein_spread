1c829e8aa5fbeaae545b8550d866a9f0d2caddf9
Christoffer Gretarsson Alexandersen <cga323@gmail.com>
2025-05-10 12:41:40 -0400

    diffgam sim

--- scripts/analyze_inference.jl
+++ scripts/analyze_inference.jl
@@ -5,9 +5,10 @@ include("helpers.jl");
 
 # simulation to analyze
 #simulation = "total_DIFFGAM_bilateral_N=40_threads=1_var1_NEWRHS";
-simulation = "main_DIFFG";
+simulation = "main_DIFFGA";
 
 # plot 
 inference_obj = deserialize("simulations/"*simulation*".jls")
+inference_obj["chain"]
 
 plot_inference(inference_obj,"figures/"*simulation;plotscale=log10)  
