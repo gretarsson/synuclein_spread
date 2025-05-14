50d952b62d8b6776f9132b29753c9a9259a100c1
Christoffer Gretarsson Alexandersen <cga323@gmail.com>
2025-05-14 16:27:32 -0400

    sims

--- scripts/analyze_inference.jl
+++ scripts/analyze_inference.jl
@@ -5,7 +5,7 @@ include("helpers.jl");
 
 # simulation to analyze
 #simulation = "total_DIFFGAM_bilateral_N=40_threads=1_var1_NEWRHS";
-simulation = "main_DIFFGAM";
+simulation = "main_DIFFGAM2";
 
 # plot 
 inference_obj = deserialize("simulations/"*simulation*".jls")
