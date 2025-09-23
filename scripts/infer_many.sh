7890351ee69c421435fbc5cc3fd3f31433c6c7e7
Christoffer Gretarsson Alexandersen <cga323@gmail.com>
2025-09-22 20:48:02 -0400

    new sims

--- scripts/infer_many.sh
+++ scripts/infer_many.sh
@@ -2,19 +2,19 @@
 set -e  # exit on first error
 
 # run inference for all models with retrograde transport
-julia --project=. scripts/infer_this_main.jl DIFF  data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFF_RETRO.jl
-julia --project=. scripts/infer_this_main.jl DIFFG data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFG_RETRO.jl
-julia --project=. scripts/infer_this_main.jl DIFFGA data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFGA_RETRO.jl
-julia --project=. scripts/infer_this_main.jl DIFFGAM data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFGAM_RETRO.jl
-
-# ...with anterograde transport
-julia --project=. scripts/infer_this_main.jl DIFF  data/W_labeled_filtered.csv  data/total_path.csv --retrograde=false --n_chains=4 --out_file=DIFF_ANTERO.jl
-julia --project=. scripts/infer_this_main.jl DIFFG data/W_labeled_filtered.csv  data/total_path.csv --retrograde=false --n_chains=4 --out_file=DIFFG_ANTERO.jl
-julia --project=. scripts/infer_this_main.jl DIFFGA data/W_labeled_filtered.csv  data/total_path.csv --retrograde=false --n_chains=4 --out_file=DIFFGA_ANTERO.jl
-julia --project=. scripts/infer_this_main.jl DIFFGAM data/W_labeled_filtered.csv  data/total_path.csv --retrograde=false --n_chains=4 --out_file=DIFFGAM_ANTERO.jl
-
-# ... and bidirectional (retrograde + anterograde)
-julia --project=. scripts/infer_this_main.jl DIFF_bidirectional  data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFF_BIDIR.jl
-julia --project=. scripts/infer_this_main.jl DIFFG_bidirectional data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFG_BIDIR.jl
-julia --project=. scripts/infer_this_main.jl DIFFGA_bidirectional data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFGA_BIDIR.jl
+#julia --project=. scripts/infer_this_main.jl DIFF  data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFF_RETRO.jl
+#julia --project=. scripts/infer_this_main.jl DIFFG data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFG_RETRO.jl
+#julia --project=. scripts/infer_this_main.jl DIFFGA data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFGA_RETRO.jl
+#julia --project=. scripts/infer_this_main.jl DIFFGAM data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFGAM_RETRO.jl
+#
+## ...with anterograde transport
+#julia --project=. scripts/infer_this_main.jl DIFF  data/W_labeled_filtered.csv  data/total_path.csv --retrograde=false --n_chains=4 --out_file=DIFF_ANTERO.jl
+#julia --project=. scripts/infer_this_main.jl DIFFG data/W_labeled_filtered.csv  data/total_path.csv --retrograde=false --n_chains=4 --out_file=DIFFG_ANTERO.jl
+#julia --project=. scripts/infer_this_main.jl DIFFGA data/W_labeled_filtered.csv  data/total_path.csv --retrograde=false --n_chains=4 --out_file=DIFFGA_ANTERO.jl
+#julia --project=. scripts/infer_this_main.jl DIFFGAM data/W_labeled_filtered.csv  data/total_path.csv --retrograde=false --n_chains=4 --out_file=DIFFGAM_ANTERO.jl
+#
+## ... and bidirectional (retrograde + anterograde)
+#julia --project=. scripts/infer_this_main.jl DIFF_bidirectional  data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFF_BIDIR.jl
+#julia --project=. scripts/infer_this_main.jl DIFFG_bidirectional data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFG_BIDIR.jl
+#julia --project=. scripts/infer_this_main.jl DIFFGA_bidirectional data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFGA_BIDIR.jl
 julia --project=. scripts/infer_this_main.jl DIFFGAM_bidirectional data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=4 --out_file=DIFFGAM_BIDIR.jl
