0cdb40d11aba3578407d06ce8c131fe5d1a90731
gretarsson <cga323@gmail.com>
2025-09-30 14:31:32 -0400

    cleaned up

--- scripts/test.jl
+++
@@ -1,9 +0,0 @@
-using Serialization
-include("Data_processing.jl")
-using .Data_processing: process_pathology
-
-data1 = deserialize("data/total_path_3D.jls")
-data2 = process_pathology("data/total_path.csv"; W_csv="data/W_labeled.csv")
-
-isequal(data1,data2)
-
