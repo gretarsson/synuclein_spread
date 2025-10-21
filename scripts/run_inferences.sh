58a092295fea02f115ffee41e710403dc80a8350
gretarsson <cga323@gmail.com>
2025-10-21 10:58:27 -0400

    m

--- scripts/run_inferences.sh
+++ scripts/run_inferences.sh
@@ -13,25 +13,25 @@ declare -A BASE_JOBS
 BASE_JOBS["DIFF_RETRO"]="DIFF data/W_labeled_filtered.csv data/total_path.csv --retrograde=true --n_chains=1"
 BASE_JOBS["DIFF_ANTERO"]="DIFF data/W_labeled_filtered.csv data/total_path.csv --retrograde=false --n_chains=1"
 BASE_JOBS["DIFF_BIDIR"]="DIFF_bidirectional data/W_labeled_filtered.csv data/total_path.csv --n_chains=1"
-BASE_JOBS["DIFF_EUCL"]="DIFF data/W_labeled_filtered.csv data/Euclidean_distance_matrix_filtered.csv --n_chains=1"
+BASE_JOBS["DIFF_EUCL"]="DIFF data/Euclidean_distance_matrix_filtered.csv data/total_path.csv --n_chains=1"
 
 # DIFFG
 BASE_JOBS["DIFFG_RETRO"]="DIFFG data/W_labeled_filtered.csv data/total_path.csv --retrograde=true --n_chains=1"
 BASE_JOBS["DIFFG_ANTERO"]="DIFFG data/W_labeled_filtered.csv data/total_path.csv --retrograde=false --n_chains=1"
 BASE_JOBS["DIFFG_BIDIR"]="DIFFG_bidirectional data/W_labeled_filtered.csv data/total_path.csv --n_chains=1"
-BASE_JOBS["DIFFG_EUCL"]="DIFFG data/W_labeled_filtered.csv data/Euclidean_distance_matrix_filtered.csv --n_chains=1"
+BASE_JOBS["DIFFG_EUCL"]="DIFFG data/Euclidean_distance_matrix_filtered.csv data/total_path.csv --n_chains=1"
 
 # DIFFGA
 BASE_JOBS["DIFFGA_RETRO"]="DIFFGA data/W_labeled_filtered.csv data/total_path.csv --retrograde=true --n_chains=1"
 BASE_JOBS["DIFFGA_ANTERO"]="DIFFGA data/W_labeled_filtered.csv data/total_path.csv --retrograde=false --n_chains=1"
 BASE_JOBS["DIFFGA_BIDIR"]="DIFFGA_bidirectional data/W_labeled_filtered.csv data/total_path.csv --n_chains=1"
-BASE_JOBS["DIFFGA_EUCL"]="DIFFGA data/W_labeled_filtered.csv data/Euclidean_distance_matrix_filtered.csv --n_chains=1"
+BASE_JOBS["DIFFGA_EUCL"]="DIFFGA data/Euclidean_distance_matrix_filtered.csv data/total_path.csv --n_chains=1"
 
 # DIFFGAM
 BASE_JOBS["DIFFGAM_RETRO"]="DIFFGAM data/W_labeled_filtered.csv data/total_path.csv --retrograde=true --n_chains=1"
 BASE_JOBS["DIFFGAM_ANTERO"]="DIFFGAM data/W_labeled_filtered.csv data/total_path.csv --retrograde=false --n_chains=1"
 BASE_JOBS["DIFFGAM_BIDIR"]="DIFFGAM_bidirectional data/W_labeled_filtered.csv data/total_path.csv --n_chains=1"
-BASE_JOBS["DIFFGAM_EUCL"]="DIFFGAM data/W_labeled_filtered.csv data/Euclidean_distance_matrix_filtered.csv --n_chains=1"
+BASE_JOBS["DIFFGAM_EUCL"]="DIFFGAM data/Euclidean_distance_matrix_filtered.csv data/total_path.csv --n_chains=1"
 
 # HELD OUT LAST TIME POINTS
 BASE_JOBS["DIFF_T1"]="DIFF data/W_labeled_filtered.csv data/total_path.csv --retrograde=true --holdout_last=1"
