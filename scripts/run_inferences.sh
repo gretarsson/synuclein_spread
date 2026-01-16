7c0479aa580c67ee893eac6aa9c3d1bebbea7a1d
gretarsson <cga323@gmail.com>
2026-01-16 14:00:54 -0500

    small edit

--- scripts/run_inferences.sh
+++ scripts/run_inferences.sh
@@ -42,10 +42,10 @@ BASE_JOBS["u0_DIFFG_BIDIR"]="DIFFG_bidirectional data/W_labeled_filtered.csv dat
 BASE_JOBS["u0_DIFFG_EUCL"]="DIFFG data/Euclidean_distance_matrix_filtered.csv data/total_path.csv --n_chains=1 --ignore_seed"
 
 # DIFFGA
-BASE_JOBS["u0_DIFFGA_RETRO"]="DIFFGA data/W_labeled_filtered.csv data/total_path.csv --retrograde=true --n_chains=1 --ignore-seed"
-BASE_JOBS["u0_DIFFGA_ANTERO"]="DIFFGA data/W_labeled_filtered.csv data/total_path.csv --retrograde=false --n_chains=1 --ignore-seed"
-BASE_JOBS["u0_DIFFGA_BIDIR"]="DIFFGA_bidirectional data/W_labeled_filtered.csv data/total_path.csv --n_chains=1 --ignore-seed"
-BASE_JOBS["u0_DIFFGA_EUCL"]="DIFFGA data/Euclidean_distance_matrix_filtered.csv data/total_path.csv --n_chains=1 --ignore-seed"
+BASE_JOBS["u0_DIFFGA_RETRO"]="DIFFGA data/W_labeled_filtered.csv data/total_path.csv --retrograde=true --n_chains=1 --ignore_seed"
+BASE_JOBS["u0_DIFFGA_ANTERO"]="DIFFGA data/W_labeled_filtered.csv data/total_path.csv --retrograde=false --n_chains=1 --ignore_seed"
+BASE_JOBS["u0_DIFFGA_BIDIR"]="DIFFGA_bidirectional data/W_labeled_filtered.csv data/total_path.csv --n_chains=1 --ignore_seed"
+BASE_JOBS["u0_DIFFGA_EUCL"]="DIFFGA data/Euclidean_distance_matrix_filtered.csv data/total_path.csv --n_chains=1 --ignore_seed"
 #
 ## DIFFGAM
 #BASE_JOBS["DIFFGAM_RETRO"]="DIFFGAM data/W_labeled_filtered.csv data/total_path.csv --retrograde=true --n_chains=1"
