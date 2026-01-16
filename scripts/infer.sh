ddaa0e63003614e4fe0f9a2c1ad1b8c710223a2e
gretarsson <cga323@gmail.com>
2026-01-16 14:16:13 -0500

    m

--- scripts/infer.sh
+++ scripts/infer.sh
@@ -2,4 +2,4 @@
 set -euo pipefail  # exit on first error
 
 # run inference!
-julia --project=. scripts/infer_this_main.jl DIFF data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=1 --seed_indices="[74]" --out_file=simulations/DIFF_u02.jls --ignore_seed
+julia --project=. scripts/infer_this_main.jl DIFFGA data/W_labeled_filtered.csv  data/total_path.csv --retrograde=true --n_chains=1 --seed_indices="[74]" --out_file=simulations/DIFF_u02.jls --ignore_seed
