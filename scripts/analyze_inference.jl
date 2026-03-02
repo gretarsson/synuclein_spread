d59a38d4ab258949533ccc581876ab8ff46b5477
gretarsson <cga323@gmail.com>
2026-03-02 10:25:02 -0500

    m

--- scripts/analyze_inference.jl
+++ scripts/analyze_inference.jl
@@ -2,7 +2,7 @@
 here we create a folder of analysis plots of interence results
 =#
 using PathoSpread
-plot_priors_and_posteriors = false
+plot_priors_and_posteriors = true
 
 
 simulations = ["igs_DIFF_EUCL", "igs_DIFF_ANTERO", "igs_DIFF_RETRO", "igs_DIFF_BIDIR",
@@ -18,7 +18,7 @@ simulations = ["hippo_DIFFGA_RETRO_posterior_prior"]
 #simulations = ["BILATERAL_DIFFG_RETRO", "BILATERAL_DIFFGA_RETRO"]
 simulations = ["DIFFGA_T1", "DIFFGA_T2", "DIFFG_T1", "DIFFG_T2"]
 
-simulations = ["new_hippo_DIFFGA_RETRO_C2"]
+simulations = ["new_hippo_DIFF_RETRO_C1"]
 
 for simulation in simulations
     # simulation to analyze
