89fee93a86d8626d5fc2971e1e33806d705a6d0f
gretarsson <cga323@gmail.com>
2026-03-08 09:36:26 -0400

    m

--- scripts/run_inferences.sh
+++ scripts/run_inferences.sh
@@ -85,9 +85,9 @@ declare -A BASE_JOBS
 ## HIPPOCAMPUS INJECTION SITE
 ## --------------------------------
 # ONLY RETRO
-BASE_JOBS["new_hippo_DIFF_RETRO"]="DIFF data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1 --large_u0 --small_sigma"
-BASE_JOBS["new_hippo_DIFFG_RETRO"]="DIFFG data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1 --small_sigma"
-BASE_JOBS["new_hippo_DIFFGA_RETRO"]="DIFFGA data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1 --small_sigma"
+#BASE_JOBS["new_hippo_DIFF_RETRO"]="DIFF data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1 --large_u0 --small_sigma"
+#BASE_JOBS["new_hippo_DIFFG_RETRO"]="DIFFG data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1 --small_sigma"
+#BASE_JOBS["new_hippo_DIFFGA_RETRO"]="DIFFGA data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1 --small_sigma"
 
 ## DIFF
 #BASE_JOBS["hippo_DIFF_RETRO"]="DIFF data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1"
@@ -132,8 +132,8 @@ BASE_JOBS["new_hippo_DIFFGA_RETRO"]="DIFFGA data/W_labeled_filtered.csv data/hip
 #
 ## HIPPO WITH PRIORS FROM STRIATUM
 #BASE_JOBS["hippo_DIFF_RETRO_posterior_prior"]="DIFF data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1 --posterior_priors='simulations/DIFF_RETRO.jls'"
-#BASE_JOBS["hippo_DIFFG_RETRO_posterior_prior"]="DIFFG data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1  --posterior_priors='simulations/DIFFG_RETRO.jls'"
-#BASE_JOBS["hippo_DIFFGA_RETRO_posterior_prior"]="DIFFGA data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1  --posterior_priors='simulations/DIFFGA_RETRO.jls'"
+BASE_JOBS["global_hippo_DIFFG_RETRO_posterior_prior"]="DIFFG data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1  --posterior_priors='simulations/DIFFG_RETRO.jls'"
+BASE_JOBS["global_hippo_DIFFGA_RETRO_posterior_prior"]="DIFFGA data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1  --posterior_priors='simulations/DIFFGA_RETRO.jls'"
 #BASE_JOBS["hippo_DIFFGAM_RETRO_posterior_prior"]="DIFFGAM data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=true --n_chains=1  --posterior_priors='simulations/DIFFGAM_RETRO.jls'"
 #
 #BASE_JOBS["hippo_DIFF_ANTERO_posterior_prior"]="DIFF data/W_labeled_filtered.csv data/hippocampal/hippocampal_syn_only.csv --seed_indices='[53,55,56]' --retrograde=false --n_chains=1 --posterior_priors='simulations/DIFF_ANTERO.jls'"
